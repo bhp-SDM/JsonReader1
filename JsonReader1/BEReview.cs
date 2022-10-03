5b1a9536fcf5d1fc10757726fe5f1194172bc255
BentHPedersen <benthpedersen@mail.com>
2022-10-03 20:41:11 +0200

    Add project files.

---
+++ JsonReader1/BEReview.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonReader1
+{
+    public class BEReview
+    {
+        public int Reviewer { get; set; }
+        public int Movie { get; set; }
+        public int Grade { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
---
+++ JsonReader1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace JsonReader1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader r = new StreamReader("../../../../ratings.json"))
+            {
+                Console.Write("Converting Json file to objects... ");
+                
+                Stopwatch sw = Stopwatch.StartNew();
+                
+                List<BEReview>? items = JsonConvert.DeserializeObject<List<BEReview>>(r.ReadToEnd());
+                if (items == null)
+                    items = new List<BEReview>();
+
+                sw.Stop();
+                
+                Console.WriteLine($"Time = {sw.Elapsed.TotalSeconds:f4} sec.");
+                Console.WriteLine($"Review Count = {items.Count:N0}");
+                Console.WriteLine("First Reviewer = " + items[0].Reviewer);
+                Console.WriteLine("Last Reviewer  = " + items[items.Count - 1].Reviewer);
+            }
+        }
+    }
+}
